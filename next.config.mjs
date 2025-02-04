2076f4a42dd6edfb95d689ad9eb07a550248a257
abel <admin@sxyseo.com>
2025-02-05 00:43:53 +0800

    cursor-rules.whatled.com

--- next.config.mjs
+++ next.config.mjs
@@ -13,40 +13,35 @@ const nextConfig = {
         serverActions: false, // 禁用服务器操作
     },
     webpack: (config, { isServer }) => {
-        config.optimization.minimize = true;
+        // 禁用持久化缓存
+        config.cache = false;
 
-        // 更激进的代码分割配置
-        config.optimization.splitChunks = {
-            chunks: 'all',
-            maxInitialRequests: 25,
-            minSize: 20000,
-            maxSize: 2000000, // 降低到 2MB
-            cacheGroups: {
-                default: false,
-                vendors: false,
-                framework: {
-                    chunks: 'all',
-                    name: 'framework',
-                    test: /(?<!node_modules.*)[\\/]node_modules[\\/](react|react-dom|scheduler|prop-types|use-subscription)[\\/]/,
-                    priority: 40,
-                    enforce: true,
-                },
-                lib: {
-                    test: /[\\/]node_modules[\\/]/,
-                    name(module, chunks, cacheGroupKey) {
-                        const moduleFileName = module.identifier().split('/').reduceRight(item => item);
-                        return `${cacheGroupKey}.${moduleFileName.replace(/[^a-zA-Z0-9]/g, '')}`;
+        config.optimization = {
+            ...config.optimization,
+            minimize: true,
+            splitChunks: {
+                chunks: 'all',
+                maxInitialRequests: 25,
+                minSize: 20000,
+                maxSize: 1000000, // 1MB
+                cacheGroups: {
+                    default: false,
+                    vendors: false,
+                    framework: {
+                        name: 'framework',
+                        test: /[\\/]node_modules[\\/](react|react-dom|scheduler)[\\/]/,
+                        priority: 40,
+                        enforce: true,
                     },
-                    priority: 30,
-                    minChunks: 1,
-                    reuseExistingChunk: true,
-                },
-                commons: {
-                    name: 'commons',
-                    minChunks: 2,
-                    priority: 20
+                    commons: {
+                        name: 'commons',
+                        minChunks: 2,
+                        priority: 20,
+                        reuseExistingChunk: true,
+                    }
                 }
-            }
+            },
+            runtimeChunk: false
         };
 
         if (!isServer) {
